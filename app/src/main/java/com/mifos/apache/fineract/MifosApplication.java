54886dcd786503cb1be434e6a240b009292d7974
Rajan Maurya <rajanmaurya154@gmail.com>
2017-03-16 15:43:19 +0530

    chore: Initialised project with MVP Architecture packages

---
+++ app/src/main/java/com/mifos/apache/fineract/MifosApplication.java
@@ -0,0 +1,15 @@
+package com.mifos.apache.fineract;
+
+import android.app.Application;
+
+/**
+ * Created by Rajan Maurya on 16/03/17.
+ */
+
+public class MifosApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/mifos/apache/fineract/ui/main/MainActivity.java
+++ app/src/main/java/com/mifos/apache/fineract/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mifos.apache.fineract;
+package com.mifos.apache.fineract.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mifos.apache.fineract.R;
 
 public class MainActivity extends AppCompatActivity {
 
