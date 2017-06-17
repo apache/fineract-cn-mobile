fcbd0b45e8797a910fb391246e018ddd3a600873
Rajan Maurya <rajanmaurya154@gmail.com>
2017-06-17 16:52:02 +0530

    Style: Manage Preference Keys

---
+++ app/src/main/java/com/mifos/apache/fineract/data/local/PreferenceKey.java
@@ -0,0 +1,12 @@
+package com.mifos.apache.fineract.data.local;
+
+/**
+ * @author Rajan Maurya
+ *         On 17/06/17.
+ */
+
+public class PreferenceKey {
+
+    public static final String TENANT_IDENTIFIER = "tenant_identifier";
+    public static final String ACCESS_TOKEN = "access_token";
+}
