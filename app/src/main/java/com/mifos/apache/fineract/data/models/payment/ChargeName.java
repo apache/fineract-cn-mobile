b5ce8aea605b4c0c6236c530431dad6c8aed95d3
Rajan Maurya <rajanmaurya154@gmail.com>
2017-07-13 15:38:45 +0530

    feat: Planned payment models

---
+++ app/src/main/java/com/mifos/apache/fineract/data/models/payment/ChargeName.java
@@ -0,0 +1,28 @@
+package com.mifos.apache.fineract.data.models.payment;
+
+/**
+ * @author Rajan Maurya
+ *         On 13/07/17.
+ */
+
+public class ChargeName {
+
+    private String identifier;
+    private String name;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
