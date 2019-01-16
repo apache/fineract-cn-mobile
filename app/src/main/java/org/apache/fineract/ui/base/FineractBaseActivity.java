9003b6891d95d70497ee16c99c68ba9542151fd9
abhijitramesh <abhijitramesh2k@gmail.com>
2019-01-16 19:11:51 +0530

    style: removed redundant type casting

--- app/src/main/java/org/apache/fineract/ui/base/FineractBaseActivity.java
+++ app/src/main/java/org/apache/fineract/ui/base/FineractBaseActivity.java
@@ -43,7 +43,7 @@ public class FineractBaseActivity extends AppCompatActivity implements BaseActiv
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar =  findViewById(R.id.toolbar);
         if (toolbar != null) {
             setSupportActionBar(toolbar);
         }
--- app/src/main/java/org/apache/fineract/ui/online/DashboardActivity.java
+++ app/src/main/java/org/apache/fineract/ui/online/DashboardActivity.java
@@ -134,7 +134,7 @@ public class DashboardActivity extends FineractBaseActivity implements
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer =  findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
