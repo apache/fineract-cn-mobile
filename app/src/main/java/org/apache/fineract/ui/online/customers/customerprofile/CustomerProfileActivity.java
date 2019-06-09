19f31130b6cc0619e674d927f1a33f437a8d53ce
miPlodder <academic.rahul@gmail.com>
2019-06-09 17:32:50 +0530

    FINCN-158: App Crash on clicking Customer Image fixed

--- app/src/main/java/org/apache/fineract/ui/online/customers/customerprofile/CustomerProfileActivity.java
+++ app/src/main/java/org/apache/fineract/ui/online/customers/customerprofile/CustomerProfileActivity.java
@@ -12,6 +12,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -45,7 +47,8 @@ public class CustomerProfileActivity extends FineractBaseActivity
     ImageView ivCustomerProfile;
 
     @BindView(R.id.layout_error)
-    ImageView errorView;
+    CoordinatorLayout errorView;
+
     private String customerIdentifier;
 
     @Override
